e7fd2a40d6448ef9cc6c482e61967925220a9578
Oyebode Adetunji <adetunji.oyebode6@gmail.com>
2023-07-01 02:45:33 +0100

    A class object with a getter and a setter attribute

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new Error('Name must be a string');
+    }
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    } else {
+      throw new Error('Length must be a number');
+    }
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students
+  set students(value) {
+    if (Array.isArray(value)) {
+      this._students = value;
+    } else {
+      throw new Error('Students must be an array');
+    }
+  }
+}
